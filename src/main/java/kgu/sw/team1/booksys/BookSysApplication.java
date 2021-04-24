175b20f3a29bc0a75f119e35215e254ea09868a8
devwithpug <zmfjscl789@gmail.com>
2021-04-24 19:03:25 +0900

    initial commit

---
+++ src/main/java/kgu/sw/team1/booksys/BookSysApplication.java
@@ -0,0 +1,13 @@
+package kgu.sw.team1.booksys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kgu/sw/team1/booksys/BookSysApplicationTests.java
@@ -0,0 +1,13 @@
+package kgu.sw.team1.booksys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
